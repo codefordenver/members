7e28d40b14e643713dbd1dd92a317806ef7b9ede
Joshua Small <jksmall0631@gmail.com>
2017-08-03 20:30:39 -0600

    Get data from Graph.Cool! (#8)
    * working on graph.cool connection
    
    * connect Apollo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import gql from "graphql-tag";
+import { ApolloProvider, graphql } from "react-apollo";
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: "https://api.graph.cool/simple/v1/cj5iz3htl74ms012245dwlq4t"
+  })
+});
+
+const Member = ({ data }) => {
+  console.log(data.allMembers);
+  return (
+    <div>
+      {JSON.stringify(data.allMembers)}
+    </div>
+  );
+};
+
+Member.defaultProps = {
+  members: []
+};
+
+const MembersWithData = graphql(
+  gql`
+    query members {
+      allMembers {
+        email
+      }
+    }
+  `,
+  { options: () => ({}) }
+)(Member);
 
 class App extends Component {
+  createClient() {
+    return client;
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <ApolloProvider client={this.createClient()}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <MembersWithData />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </ApolloProvider>
     );
   }
 }
