fe82bd431fa58da6b0768d8bcddd862d6cee536d
Joshua Small <jksmall0631@gmail.com>
2017-08-07 22:10:54 -0600

    install auth0-lock and add clientID and domain as props for LoginAuth0

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import ApolloClient, { createNetworkInterface } from "apollo-client";
 import gql from "graphql-tag";
 import { ApolloProvider, graphql } from "react-apollo";
 import MemberResources from "./MemberResources";
+import LoginAuth0 from "./LoginAuth0";
 import "./App.css";
 
-const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: "https://api.graph.cool/simple/v1/cj5iz3htl74ms012245dwlq4t"
-  })
-
-  networkInterface.use([{
-    applyMiddleware (req, next) {
-      if (!req.options.headers) {
-        req.options.headers = {}
-      }
-
-      if (localStorage.getItem('auth0IdToken')) {
-        req.options.headers.authorization = `Bearer ${localStorage.getItem('auth0IdToken')}`
-      }
-      next()
-    },
-  }])
+const networkInterface = createNetworkInterface({
+  uri: "https://api.graph.cool/simple/v1/cj5iz3htl74ms012245dwlq4t"
 });
+
+networkInterface.use([
+  {
+    applyMiddleware(req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {};
+      }
+
+      if (localStorage.getItem("auth0IdToken")) {
+        req.options.headers.authorization = `Bearer ${localStorage.getItem(
+          "auth0IdToken"
+        )}`;
+      }
+      next();
+    }
+  }
+]);
+
+const client = new ApolloClient({ networkInterface });
 
 const Member = ({ members }) => {
   return (
@@ -67,6 +72,10 @@ class App extends Component {
         <div className="App">
           <div className="App-header">
             <h2>Code for Denver Members</h2>
+            <LoginAuth0
+              clientId="Qgo5BOv5XsU60A6mFSjmZpzfTrYy8I5I"
+              domain="codefordenver.auth0.com"
+            />
           </div>
           <MembersWithData />
           <MemberResources />
