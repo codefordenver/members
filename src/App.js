b0f905a65aa3c7fd7a2017765063208863161298
Joshua Small <jksmall0631@gmail.com>
2017-08-07 20:58:23 -0600

    create LoginAuth0 component and add conditional to Apollo client

--- src/App.js
+++ src/App.js
@@ -1,48 +1,79 @@
 import React, { Component } from "react";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
 import gql from "graphql-tag";
-import { graphql } from "react-apollo";
+import { ApolloProvider, graphql } from "react-apollo";
 import MemberResources from "./MemberResources";
-import LoginAuth0 from "./LoginAuth0";
-import MembersWithData from "./Members";
 import "./App.css";
 
-class App extends Component {
-  _isLoggedIn = () => {
-    return this.props.data.user;
-  };
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: "https://api.graph.cool/simple/v1/cj5iz3htl74ms012245dwlq4t"
+  })
 
-  _logout = () => {
-    window.localStorage.removeItem("cfd-members-auth0IdToken");
-    window.location.reload();
-  };
+  networkInterface.use([{
+    applyMiddleware (req, next) {
+      if (!req.options.headers) {
+        req.options.headers = {}
+      }
+
+      if (localStorage.getItem('auth0IdToken')) {
+        req.options.headers.authorization = `Bearer ${localStorage.getItem('auth0IdToken')}`
+      }
+      next()
+    },
+  }])
+});
+
+const Member = ({ members }) => {
+  return (
+    <div>
+      {members.map(member =>
+        <div key={member.email}>
+          {member.email}
+        </div>
+      )}
+    </div>
+  );
+};
+
+Member.defaultProps = {
+  members: []
+};
+
+const MembersWithData = graphql(
+  gql`
+    query members {
+      allMembers {
+        email
+      }
+    }
+  `,
+  {
+    options: () => ({}),
+    props: ({ data }) => {
+      return { members: data.allMembers };
+    }
+  }
+)(Member);
+
+class App extends Component {
+  createClient() {
+    return client;
+  }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Code for Denver Members</h2>
-          {!this._isLoggedIn()
-            ? <LoginAuth0
-                clientId="Qgo5BOv5XsU60A6mFSjmZpzfTrYy8I5I"
-                domain="codefordenver.auth0.com"
-              />
-            : <span onClick={this._logout}>Log out</span>}
+      <ApolloProvider client={this.createClient()}>
+        <div className="App">
+          <div className="App-header">
+            <h2>Code for Denver Members</h2>
+          </div>
+          <MembersWithData />
+          <MemberResources />
         </div>
-        <MembersWithData />
-        <MemberResources />
-      </div>
+      </ApolloProvider>
     );
   }
 }
 
-const userQuery = gql`
-  query {
-    user {
-      id
-    }
-  }
-`;
-
-export default graphql(userQuery, { options: { fetchPolicy: "network-only" } })(
-  App
-);
+export default App;
