4cb5279a7a60ecc21ff2f278f4e68578795b7aa9
Joshua Small <jksmall0631@gmail.com>
2017-08-08 23:40:02 -0600

    Port members over to its own component

---
+++ src/Members.js
@@ -0,0 +1,37 @@
+import React from "react";
+import gql from "graphql-tag";
+import { ApolloProvider, graphql } from "react-apollo";
+
+const Member = ({ members }) => {
+  return (
+    <div>
+      {members.map(member =>
+        <div key={member.email}>
+          {member.email}
+        </div>
+      )}
+    </div>
+  );
+};
+
+Member.defaultProps = {
+  members: []
+};
+
+const MembersWithData = graphql(
+  gql`
+    query members {
+      allMembers {
+        email
+      }
+    }
+  `,
+  {
+    options: () => ({}),
+    props: ({ data }) => {
+      return { members: data.allMembers };
+    }
+  }
+)(Member);
+
+export default MembersWithData;
