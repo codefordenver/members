7e1ad54c2a26a9f3cf84ee9111d5a54473dc4aef
Joshua Small <jksmall0631@gmail.com>
2017-08-10 00:03:31 -0600

    Create CreateUser component with gql mutation and query

--- src/LoginAuth0.js
+++ src/LoginAuth0.js
@@ -1,33 +1,29 @@
-import React, {Component} from 'react';
-import Auth0Lock from 'auth0-lock';
+import React, { Component } from "react";
+import Auth0Lock from "auth0-lock";
 
 export default class LoginAuth0 extends Component {
   constructor(props) {
     super(props);
 
-    this._lock = new Auth0Lock(props.clientId, props.domain)
+    this._lock = new Auth0Lock(props.clientId, props.domain);
   }
 
   componentDidMount() {
-    this._lock.on('authenticated', (authResult) => {
-      window.localStorage.setItem('auth0IdToken', authResult.idToken)
+    this._lock.on("authenticated", authResult => {
+      window.localStorage.setItem("auth0IdToken", authResult.idToken);
       // this.props.router.replace(`/login`)
-    })
+    });
   }
 
   _showLogin = () => {
-    this._lock.show()
-  }
+    this._lock.show();
+  };
 
   render() {
     return (
       <div>
-        <span
-          onClick={this._showLogin}
-        >
-          Log in with Auth0
-        </span>
+        <span onClick={this._showLogin}>Log in with Auth0</span>
       </div>
-    )
+    );
   }
 }
