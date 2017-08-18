3d8dcd08c84c7582fbe951ea3121d1f758fd290f
Joshua Small <jksmall0631@gmail.com>
2017-08-17 19:03:29 -0600

    wip: create users

--- src/App.js
+++ src/App.js
@@ -1,71 +1,31 @@
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import MemberResources from "./MemberResources";
 import LoginAuth0 from "./LoginAuth0";
 import MembersWithData from "./Members";
+import CreateUser from "./CreateUser";
 import "./App.css";
 
-const foo = (name, email) => {
-  return createMember({
-    variables : {
-      name,
-      email
-    }
-  });
-}
-
-const requestMembersData = () => {
-  const id_token = window.localStorage.getItem("cfd-members-auth0IdToken");
-  fetch("https://codefordenver.auth0.com/tokeninfo", {
-    method: "POST",
-    mode: "cors",
-    redirect: "follow",
-    headers: new Headers({
-      "Content-Type": "application/json"
-    }),
-    body: JSON.stringify({
-      id_token
-    })
-  })
-    .then(data => data.json())
-    .then(json => {
-      try {
-        const f = foo(json.name, json.email);
-        console.log(f);
-      } catch (e) {
-        console.log(e);
-      }
-
-    });
-};
-
-const createMemberQuery = gql`
-  mutation($name: String!, $email: String!) {
-    createMember(
-      name: $name,
-      email: $email
-    ) {
-      id
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: ""
     }
   }
-`
- const createMember = graphql(createMemberQuery, { name: 'createMember' })
 
-class App extends Component {
-  _isLoggedIn = () => {
+  _isLoggedIn() {
     return this.props.data.user;
   };
 
-  _logout = () => {
+  _logout() {
     window.localStorage.removeItem("cfd-members-auth0IdToken");
     window.location.reload();
   };
 
   render() {
-    if (!this.props.data.user) {
-      requestMembersData();
-    }
     return (
       <div className="App">
         <div className="App-header">
@@ -79,10 +39,11 @@ class App extends Component {
         </div>
         <MembersWithData />
         <MemberResources />
+        {/* <Route pathname="/signup" component={ CreateUser } /> */}
       </div>
     );
   }
-};
+}
 
 const userQuery = gql`
   query {
@@ -92,8 +53,8 @@ const userQuery = gql`
   }
 `;
 
-export default graphql(userQuery, { options: { fetchPolicy: "network-only" } })(
-  App
-);
-let app = App;
-window.app = app;
+export default graphql(userQuery, {
+  options: {
+    fetchPolicy: "network-only"
+  }
+})(App);
--- src/LoginAuth0.js
+++ src/LoginAuth0.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 import Auth0Lock from "auth0-lock";
 
-export default class LoginAuth0 extends Component {
+class LoginAuth0 extends Component {
   constructor(props) {
     super(props);
-
     this._lock = new Auth0Lock(props.clientId, props.domain);
   }
 
   componentDidMount() {
+    //localStorage is being set after the request is sent to check if a user is in DB.
     this._lock.on("authenticated", authResult => {
-      window.localStorage.setItem("cfd-members-auth0IdToken", authResult.idToken);
-      // We may want to route to /login here
-      
+      window.localStorage.setItem(
+        "cfd-members-auth0IdToken",
+        authResult.idToken
+      );
+
+      this._lock.getUserInfo(authResult.accessToken, (error, profile) => {
+        this.props.history.push({
+          pathname: '/signup',
+          state: { user: profile}
+        })
+      })
+      // window.location.reload()
     });
   }
 
@@ -21,8 +33,8 @@ export default class LoginAuth0 extends Component {
   };
 
   render() {
-    return (
-      <span onClick={this._showLogin}>Log in with Auth0</span>
-    );
+    return <span onClick={this._showLogin}>Log in with Auth0</span>;
   }
 }
+
+export default withRouter(LoginAuth0);
