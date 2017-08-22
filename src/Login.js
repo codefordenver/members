e58dcb1e6c5a77bd655d9e7f0cba8c5804e00b01
Joshua Small <jksmall0631@gmail.com>
2017-08-21 20:56:07 -0600

    wip: move conditional login buttons to Login component

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from "react";
+import LoginAuth0 from "./LoginAuth0";
 
-const Login = () => {
-  return (
-      <LoginAuth0
-        clientId="Qgo5BOv5XsU60A6mFSjmZpzfTrYy8I5I"
-        domain="codefordenver.auth0.com"
-      />
-      <p>Hello, {this.props.data.user.name}</p>
-    </div>
-  )
-}
+
+export default class Login extends Component {
+
+  _isLoggedIn() {
+    return this.props.user;
+  }
+
+  _logout() {
+    window.localStorage.removeItem("cfd-members-auth0IdToken");
+    window.location.reload();
+  }
+
+  render() {
+    if(!this._isLoggedIn()) {
+      return (
+        <LoginAuth0
+          clientId="Qgo5BOv5XsU60A6mFSjmZpzfTrYy8I5I"
+          domain="codefordenver.auth0.com"
+        />
+      )
+    } else {
+      return (
+        <div>
+          <span onClick={this._logout}>Log out</span>
+          <p>
+            Hello, { this.props.user.name }
+          </p>
+        </div>
+      );
+    }
+  }
+};
