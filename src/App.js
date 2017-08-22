5d9ff1780c53c76771a81c748dcd8f69bb44daa3
Joshua Small <jksmall0631@gmail.com>
2017-08-21 20:56:07 -0600

    wip: move conditional login buttons to Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom';
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import MemberResources from "./MemberResources";
-import LoginAuth0 from "./LoginAuth0";
+import Login from "./Login";
 import MembersWithData from "./Members";
 
 import "./App.css";
@@ -13,29 +12,15 @@ class App extends Component {
     super();
     this.state = {
       token: ""
-    }
+    };
   }
-
-  _isLoggedIn() {
-    return this.props.data.user;
-  };
-
-  _logout() {
-    window.localStorage.removeItem("cfd-members-auth0IdToken");
-    window.location.reload();
-  };
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Code for Denver Members</h2>
-          {!this._isLoggedIn()
-            ? <LoginAuth0
-                clientId="Qgo5BOv5XsU60A6mFSjmZpzfTrYy8I5I"
-                domain="codefordenver.auth0.com"
-              />
-            : <span onClick={this._logout}>Log out</span>}
+          <Login user={this.props.data.user} />
         </div>
         <MembersWithData />
         <MemberResources />
@@ -47,7 +32,8 @@ class App extends Component {
 const userQuery = gql`
   query {
     user {
-      id
+      id,
+      name
     }
   }
 `;
