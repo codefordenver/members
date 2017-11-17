14977957572e8a2159a6bc9956fab8bbfddb10c9
jpb55116 <johnbrennan@John-Brennans-MacBook-Pro.local>
2017-11-16 21:31:20 -0700

    Flexbox Changes, Material UI
    Flexbox edits, configuring “user experience” class, imported Roboto
    Condensed font

--- src/MemberProfile.js
+++ src/MemberProfile.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
 import Chip from 'material-ui/Chip';
 import FontIcon from 'material-ui/FontIcon';
+import AppBar from 'material-ui/AppBar';
 import SvgIconFace from 'material-ui/svg-icons/action/face';
 
 const MemberProfile = ({ user }) => {
@@ -13,38 +14,67 @@ const MemberProfile = ({ user }) => {
       <div className="card">
         <div className="pic">
           <div className="container-nested">
-          <img src="http://placehold.it/200x200"/>
+            <img src="http://placehold.it/225x225"/>
           </div>
         </div>
         <div className="user-bio">
           <div className="container-nested">
-        <h2>FirstName LastName</h2>
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-        culpa qui officia deserunt mollit anim id est laborum."</p>
-       <a href="mailto:email@gmail.com">email@gmail.com</a>
-          <div>
-            <span>GitHub: </span><a href="github.com">github.com</a>
+            <h2>FirstName LastName</h2>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+            culpa qui officia deserunt mollit anim id est laborum."</p>
+            <a href="mailto:email@gmail.com">email@gmail.com</a>
+              <div className= "contact">
+                <span>GitHub: </span><a href="github.com">github.com</a>
+              </div>
+              <div className= "contact">
+                <span>Flowdock: </span><a href= "flowdock.com">flowdock.com</a>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Flowdock: </span><a href= "flowdock.com">flowdock.com</a>
-          </div>
-          </div>
+    <div className="user-experience">
+      <div className="contributions-nested">
+        <div className= "contributions">
+          <p>
+            <span>
+              <div className="number">
+                150
+              </div>
+            </span> 
+              <div className="details">
+              hours contributed
+              </div>
+          </p>
         </div>
-      <div className="user-experience">
-        <div className="container-nested">
-        <table>
-          <tr>
-            <td><span>150</span> hours contributed</td>
-            <td><span>5</span> hours contributed</td>
-            <td><span>35</span> meetups attended</td>
-          </tr>
-        </table>
-      </div>
+        <div className= "contributions">
+          <p>
+            <span>
+              <div className="number">
+                5
+              </div>
+            </span> 
+              <div className="details">
+              projects completed
+              </div>
+          </p>
+        </div>
+        <div className= "contributions">
+          <p>
+            <span>
+              <div className="number">
+                35
+              </div>
+            </span> 
+              <div className="details">
+              meetups attended
+              </div>
+          </p>
+        </div>
       </div>
     </div>
+  </div>
 
       <div className="card">
         <div className="skillsleft">
