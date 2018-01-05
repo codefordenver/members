05bb46756778b2e97f0ac26d77849237134366f1
John Brennan <jpb55116@gmail.com>
2018-01-04 21:16:50 -0700

    Add Avatar to list

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import Avatar from 'material-ui/Avatar';
+
 
 const UsersList = ({ users }) => {
   return (
     <div>
       {users.map(user =>
         <div key={user.id}>
+          <Avatar src={user.picture}/>
           {user.name}
         </div>
       )}
