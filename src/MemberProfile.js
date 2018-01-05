dd3e0a1cefff87c6fe55a39c5c8a4e714fc27603
jpb55116 <johnbrennan@John-Brennans-MacBook-Pro.local>
2018-01-04 19:34:40 -0700

    Allow all components to use Material-UI

--- src/MemberProfile.js
+++ src/MemberProfile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Chip from 'material-ui/Chip';
 
 const MemberProfile = ({user}) => {
@@ -12,118 +11,116 @@ const MemberProfile = ({user}) => {
   }
   const {picture, name, description, githubName, flowdockName, email} = user;
   return (
-    <MuiThemeProvider>
-      <div className="container">
-        <div className="card">
-          <div className="pic">
-            <div className="container-nested">
-              <img className="rounded" src={picture} alt="avatar"/>
-            </div>
+    <div className="container">
+      <div className="card">
+        <div className="pic">
+          <div className="container-nested">
+            <img className="rounded" src={picture} alt="avatar"/>
           </div>
-          <div className="user-bio">
-            <div className="container-nested">
-              <h2> {name} </h2>
-              <p>{description}</p>
-              <a href={`mailto:${email}`}>email</a>
-              <div className="contact">
-                <span className="social-links">GitHub: </span>
-                <a href={`github.com/${githubName}`}>{githubName}</a>
-              </div>
-              <div className="contact">
-                <span className="social-links">Flowdock: </span>
-                <a href={`flowdock.com/${flowdockName}`}>{flowdockName}</a>
-              </div>
+        </div>
+        <div className="user-bio">
+          <div className="container-nested">
+            <h2> {name} </h2>
+            <p>{description}</p>
+            <a href={`mailto:${email}`}>email</a>
+            <div className="contact">
+              <span className="social-links">GitHub: </span>
+              <a href={`github.com/${githubName}`}>{githubName}</a>
             </div>
-          </div>
-          <div className="user-experience">
-            <div className="contributions-nested">
-              <div className="first-child">
-                <div>
-                  <span className="hours-contributed">
-                    1
-                  </span>
-                  <div className="details">
-                    hours contributed
-                  </div>
-                </div>
-              </div>
-              <div className="second-child">
-                <div>
-                  <span className="projects-completed">
-                      5
-                  </span>
-                  <div className="details">
-                    projects completed
-                  </div>
-                </div>
-              </div>
-              <div className="last-child">
-                <div>
-                  <span className="hours-contributed">
-                      35
-                  </span>
-                  <div className="details">
-                    meetups attended
-                  </div>
-                </div>
-              </div>
+            <div className="contact">
+              <span className="social-links">Flowdock: </span>
+              <a href={`flowdock.com/${flowdockName}`}>{flowdockName}</a>
             </div>
           </div>
         </div>
-
-        <div className="card">
-          <div className="skillsleft">
-            <h2>Skills</h2>
-          </div>
-          <div className="skillsright">
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              Python
-            </Chip>
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              SQL
-            </Chip>
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              Enterprise Geodatabases
-            </Chip>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="skillsleft">
-            <h2>Projects</h2>
-          </div>
-          <div className="skillsright">
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              Python
-            </Chip>
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              SQL
-            </Chip>
-            <Chip className="skills-button"
-                  onRequestDelete={() => {
-                    return false
-                  }}>
-              Enterprise Geodatabases
-            </Chip>
+        <div className="user-experience">
+          <div className="contributions-nested">
+            <div className="first-child">
+              <div>
+                <span className="hours-contributed">
+                  1
+                </span>
+                <div className="details">
+                  hours contributed
+                </div>
+              </div>
+            </div>
+            <div className="second-child">
+              <div>
+                <span className="projects-completed">
+                    5
+                </span>
+                <div className="details">
+                  projects completed
+                </div>
+              </div>
+            </div>
+            <div className="last-child">
+              <div>
+                <span className="hours-contributed">
+                    35
+                </span>
+                <div className="details">
+                  meetups attended
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </MuiThemeProvider>
+
+      <div className="card">
+        <div className="skillsleft">
+          <h2>Skills</h2>
+        </div>
+        <div className="skillsright">
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            Python
+          </Chip>
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            SQL
+          </Chip>
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            Enterprise Geodatabases
+          </Chip>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="skillsleft">
+          <h2>Projects</h2>
+        </div>
+        <div className="skillsright">
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            Python
+          </Chip>
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            SQL
+          </Chip>
+          <Chip className="skills-button"
+                onRequestDelete={() => {
+                  return false
+                }}>
+            Enterprise Geodatabases
+          </Chip>
+        </div>
+      </div>
+    </div>
   );
 };
 
