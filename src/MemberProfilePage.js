bcdf87a7021ae1b575e8301bd900b88fc0154a4a
Will Douglas <willygdouglas@gmail.com>
2018-01-22 22:00:29 -0500

    Initial commit for being able to view profiles of other users (#99)

---
+++ src/MemberProfilePage.js
@@ -0,0 +1,23 @@
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import MemberProfile from './MemberProfile';
+
+const userQuery = gql`
+  query getUser($id: ID) {
+    User(id: $id) {
+      picture
+      name
+      description
+      githubName
+      flowdockName
+      email
+    }
+  }
+`;
+
+const MemberProfileWithData = graphql(userQuery, {
+  options: (props) => ({ variables: { id: props.match.params.id } }),
+  props: ({ data: { User } }) => ({ user: User })
+})(MemberProfile);
+
+export default MemberProfileWithData;
