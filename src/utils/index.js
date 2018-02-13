1d0058c6250be470f2530111f2c21dc9782a4bba
Willy <willygdouglas@gmail.com>
2018-02-12 17:09:37 -0700

    Add initial google analytics setup

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,6 @@ export {
 } from './componentWithLoggedInUser';
 export { default as getEnvironmentVariables } from './getEnvironmentVariables';
 export { default as MarkdownRenderer } from './MarkdownRenderer';
+export {
+  default as GoogleAnalyticsPageTracker
+} from './GoogleAnalyticsPageTracker';
