035bbe7f4f2979e2253953241e64f90c224ba397
John Brennan <jpb55116@gmail.com>
2018-02-15 21:16:13 -0700

    Update app bar (layout)

--- src/Header.js
+++ src/Header.js
@@ -4,24 +4,33 @@ import Login from './Login';
 import './Header.css';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import Grid from 'material-ui/Grid';
 
 const Header = ({ user, isAuthenticated }) => (
   <AppBar position="fixed">
     <Toolbar>
-      <a href="/">
-        <img
-          className="Header-logo"
-          src="images/cfd-circle-white.png"
-          alt="code for denver logo"
-        />
-      </a>
-      <Login user={user} isAuthenticated={isAuthenticated} />
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <a href="/">
+            <img
+              className="Header-logo"
+              src="images/cfd-circle-white.png"
+              alt="code for denver logo"
+            />
+          </a>
+        </Grid>
 
-      {isAuthenticated && (
-        <Link className="Header-link" to="/volunteers">
-          All Users
-        </Link>
-      )}
+        {isAuthenticated && (
+          <Grid item>
+            <Link className="Header-link" to="/volunteers">
+              All Users
+            </Link>
+          </Grid>
+        )}
+        <Grid item>
+          <Login isAuthenticated={isAuthenticated} user={user} />
+        </Grid>
+      </Grid>
     </Toolbar>
   </AppBar>
 );
