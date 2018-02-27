0ea87df286f19c3fb2890ae880fa1647c81ca889
John Brennan <jpb55116@gmail.com>
2018-02-26 20:30:06 -0700

    Styling User List
    Center Justify, font changes, font color changes

--- src/pages/UsersListPage.js
+++ src/pages/UsersListPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import Avatar from 'material-ui/Avatar';
+import './UsersListPage.css';
 import { Link } from 'react-router-dom';
 
 const UsersList = ({ users }) => {
   return (
     <ul>
       {users.map(user => (
-        <li key={user.id}>
+        <li className="userid" key={user.id}>
           <Link to={`/volunteers/${user.id}`}>
             <Avatar src={user.picture} />
             {user.name}
