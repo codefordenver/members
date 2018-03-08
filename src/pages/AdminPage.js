5ae7be0c8297ecd14eb3c9a227b489ad13f7a103
Willy <willygdouglas@gmail.com>
2018-03-07 19:55:29 -0700

    Add info for creating new projects

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -9,6 +9,13 @@ const AdminPage = () => (
       <Link to="/admin/onboarding">this page</Link> to get a list of people that
       signed up in the last week.
     </p>
+    <p>
+      For creating new projects go to:{' '}
+      <Link to="/projects/create">/projects/create</Link>
+      <br />
+      Technically anyone can do this right now, but we haven't made the url
+      discoverable until we figure out the flow better.
+    </p>
   </div>
 );
 
