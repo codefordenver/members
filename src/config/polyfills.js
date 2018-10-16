ef9c8d9dc94a7540e44da06c9ef1ae59470fc588
Daniel Chao <daniel@danielchao.me>
2018-10-15 20:12:30 -0600

    Fix website in microsoft edge via fetch-headers polyfill

---
+++ src/config/polyfills.js
@@ -0,0 +1,3 @@
+global.Headers = require('fetch-headers');
+// This is necessary for IE / Microsoft Edge because of a problem with apollo-link-rest
+// https://github.com/apollographql/apollo-link-rest/issues/93
