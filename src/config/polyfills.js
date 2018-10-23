9fe059cf531f10ecd9fe8cf4bb7f573b35082bd0
Willy Douglas <wdouglas@rallydev.com>
2018-10-22 20:30:37 -0600

    Add a forEach method that the polyfill doesn't support

--- src/config/polyfills.js
+++ src/config/polyfills.js
@@ -1,3 +1,12 @@
-global.Headers = require('fetch-headers');
+import Headers from 'fetch-headers';
+
 // This is necessary for IE / Microsoft Edge because of a problem with apollo-link-rest
 // https://github.com/apollographql/apollo-link-rest/issues/93
+window.Headers = Headers;
+
+// The polyfill doesn't provide a forEach function which apollo rest uses
+window.Headers.prototype.forEach = function(fn) {
+  for (let [key, value] of this.entries()) {
+    fn(value, key, this);
+  }
+};
