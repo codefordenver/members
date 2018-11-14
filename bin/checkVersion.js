aae8f434489ede9ff6d9f7b5d0742f06c0ddfc2f
cvanem <chris@greenlinkservices.com>
2018-11-13 17:05:38 -0700

    Update checkVersion to use compare-versions and remove preinstall script

--- bin/checkVersion.js
+++ bin/checkVersion.js
@@ -1,41 +1,22 @@
 /*eslint no-console: "off"*/
 const process = require('process');
 const config = require('../package.json');
-const comparator = config.engines.node.match(/[^0-9.]+/).toString();
-const minVersion = process.version.match(/[0-9.]+/).toString();
-const version = config.engines.node.match(/[0-9.]+/).toString();
+const version = process.version.match(/[0-9.]+/).toString();
+const minVersion = config.engines.node.match(/[0-9.]+/).toString();
+const compareVersions = require('compare-versions');
 
-function compareVersions(left, right) {
-  if (left === right) return 0;
-  const a = left.split('.').map(Number);
-  const b = right.split('.').map(Number);
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > b[i]) {
-      return 1;
-    }
-    if (a[i] < b[i]) {
-      return -1;
-    }
-  }
-  return true;
-}
+const comparators = [
+  { val: '>', validate: (r) => r == 1 },
+  { val: '>=', validate: (r) => r == 0 || r == 1 },
+];
 
-if (comparator !== '>' && comparator !== '>=') {
-  console.log('Error: invalid Node version comparator, should contain > || >=');
-  process.exit(1);
-}
+const comparator = comparators.find(c => c.val == config.engines.node.match(/[^0-9.]+/).toString());
+const result = comparator && comparator.validate(compareVersions(version, minVersion));
 
-const comparison = compareVersions(version, minVersion);
+console.log(result === undefined ?
+  'Error: invalid Node version comparator, should contain ' + comparators.map(c => c.val).join(' || ')
+  : result ? `Notice: Node version check ${version} ${comparator.val} ${minVersion} good`
+    : `Error: Node version ${version} not ${comparator.val} ${minVersion}`
+);
 
-const success =
-  comparator === '>=' ? comparison === 0 || comparison === 1 : comparison === 1;
-
-if (success) {
-  console.log(
-    `Notice: Node version check ${minVersion} ${comparator} ${version} good`
-  );
-  process.exit(0);
-} else {
-  console.log(`Error: Node version ${minVersion} not ${comparator} ${version}`);
-  process.exit(1);
-}
+process.exit(!result);
