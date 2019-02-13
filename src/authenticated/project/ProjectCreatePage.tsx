ca2124d8495ab7889cfead47ea82ceee22ee0bc4
Willy Douglas <wdouglas@rallydev.com>
2019-02-13 16:16:27 -0700

    Spike adopting Formik for forms

--- src/authenticated/project/ProjectCreatePage.tsx
+++ src/authenticated/project/ProjectCreatePage.tsx
@@ -1,46 +1,160 @@
-import { compose } from 'react-apollo';
-import ProjectSection from './ProjectSection';
-import withCreatePage from '../../utils/withCreatePage';
+import React from 'react';
+import { MutationFn } from 'react-apollo';
+// import withCreatePage from '../../utils/withCreatePage';
 import {
-  CreateProjectHOC,
-  ProjectSectionFieldsFragment
+  Link,
+  withRouter,
+  RouteComponentProps,
+  Prompt
+} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { Formik } from 'formik';
+import { History } from 'history';
+import ProjectSection from './ProjectSection';
+import {
+  // CreateProjectHOC,
+  CreateProjectComponent,
+  ProjectSectionFieldsFragment,
+  CreateProjectMutation,
+  CreateProjectVariables
 } from '../../generated-models';
 
-const ProjectCreatePage = compose(
-  CreateProjectHOC({
-    props: ({ mutate }) => ({
-      onCreate: (newProject: ProjectSectionFieldsFragment) => {
-        if (!newProject.repoName) {
-          throw new Error('Projects require a specified repo name');
-        }
-        const { skills, champions, ...newProjectSansUnusedFields } = newProject;
+function getBaseUrl(history: History) {
+  return history.location.pathname.split('/create')[0];
+}
 
-        return (
-          mutate &&
-          mutate({
-            variables: {
-              ...newProjectSansUnusedFields,
-              skillsIds: newProject.skills
-                ? newProject.skills.map(skill => skill.id)
-                : [],
-              championsIds: newProject.champions
-                ? newProject.champions.map(champion => champion.id)
-                : [],
-              repoName: newProject.repoName // Typescript was complaining without this
-            }
-          })
-        );
+// interface ProjectCreatePageProps {
+//   history: History;
+// }
+
+const createProject = async (
+  createProjectMutation: MutationFn<
+    CreateProjectMutation,
+    CreateProjectVariables
+  >,
+  history: History,
+  newProject: ProjectSectionFieldsFragment,
+  actions: { setSubmitting(val: boolean): void }
+) => {
+  try {
+    if (!newProject.repoName) {
+      throw new Error('Projects require a specified repo name');
+    }
+    const { skills, champions, ...newProjectSansUnusedFields } = newProject;
+
+    await createProjectMutation({
+      variables: {
+        ...newProjectSansUnusedFields,
+        skillsIds: newProject.skills
+          ? newProject.skills.map(skill => skill.id)
+          : [],
+        championsIds: newProject.champions
+          ? newProject.champions.map(champion => champion.id)
+          : [],
+        repoName: newProject.repoName // Typescript was complaining without this
       }
-    }),
-    options: {
-      refetchQueries: ['projects']
-    }
-  }),
-  withCreatePage({
-    renameProps: {
-      formData: 'project'
-    }
-  })
-)(ProjectSection);
+    });
+    history.push(getBaseUrl(history));
+  } catch (err) {
+    actions.setSubmitting(false);
+    alert(err);
+  }
+};
 
-export default ProjectCreatePage;
+const ProjectCreatePage: React.SFC<RouteComponentProps<{}>> = ({ history }) => {
+  return (
+    <CreateProjectComponent>
+      {createProjectMutation => (
+        <Formik
+          initialValues={{
+            id: '',
+            name: '',
+            headerImage: '',
+            description: '',
+            repoName: '',
+            skills: [],
+            champions: []
+          }}
+          onSubmit={(newProject, actions) =>
+            createProject(createProjectMutation, history, newProject, actions)
+          }
+          render={({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <ProjectSection
+                project={values}
+                editing={true}
+                onFormDataChange={handleChange}
+              />
+
+              <Prompt
+                when={touched && !isSubmitting}
+                message="Warning, you have unsaved changes. Are you sure you want to leave?"
+              />
+
+              <Button
+                component={({ innerRef, ...props }) => (
+                  <Link to={getBaseUrl(history)} {...props} />
+                )}
+              >
+                Cancel
+              </Button>
+              <Button onClick={() => handleSubmit()} disabled={isSubmitting}>
+                Create
+              </Button>
+            </form>
+          )}
+        />
+      )}
+    </CreateProjectComponent>
+  );
+};
+
+export default withRouter(ProjectCreatePage);
+
+// const ProjectCreatePage = compose(
+//   CreateProjectHOC({
+//     props: ({ mutate }) => ({
+//       onCreate: (newProject: ProjectSectionFieldsFragment) => {
+//         if (!newProject.repoName) {
+//           throw new Error('Projects require a specified repo name');
+//         }
+//         const { skills, champions, ...newProjectSansUnusedFields } = newProject;
+
+//         return (
+//           mutate &&
+//           mutate({
+//             variables: {
+//               ...newProjectSansUnusedFields,
+//               skillsIds: newProject.skills
+//                 ? newProject.skills.map(skill => skill.id)
+//                 : [],
+//               championsIds: newProject.champions
+//                 ? newProject.champions.map(champion => champion.id)
+//                 : [],
+//               repoName: newProject.repoName // Typescript was complaining without this
+//             }
+//           })
+//         );
+//       }
+//     }),
+//     options: {
+//       refetchQueries: ['projects']
+//     }
+//   }),
+//   withCreatePage({
+//     renameProps: {
+//       formData: 'project'
+//     }
+//   })
+// )(ProjectSection);
+
+// export default ProjectCreatePage;
