43faacf2d47ad094abeb2d797c96a473b9735f56
Patrick <patrick@anotherpatrickcollins.com>
2019-04-01 21:51:09 -0600

    Update LandingPage.tsx

--- src/unauthenticated/LandingPage.tsx
+++ src/unauthenticated/LandingPage.tsx
@@ -4,13 +4,13 @@ import './LandingPage.css';
 
 const LandingPage = () => (
   <div>
-    <h1>Welcome to the Code for Denver Members Site</h1>
+    <h1 className="leftPad">Welcome to the Code for Denver Members Site</h1>
     <img
       className="LandingPage-members-pic"
       src={membersPic}
       alt="Some of the members of Code for Denver"
     />
-    <p>Please sign up or log in</p>
+    <p className="leftPad">Please sign up or log in</p>
   </div>
 );
 
