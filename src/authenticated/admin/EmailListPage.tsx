44ee4cbd0d2f43e928be2be569f08ccdf7dcceb8
Will Douglas <willygdouglas@gmail.com>
2019-06-03 23:56:41 -0600

    Convert EmailListPage to use react-apollo-hooks

--- src/authenticated/admin/EmailListPage.tsx
+++ src/authenticated/admin/EmailListPage.tsx
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { format, subDays } from 'date-fns';
 import LoadingIndicator from '../../shared-components/LoadingIndicator';
-import { UserEmailsHOC, UserEmailsAllUsers } from '../../generated-models';
+import { useQuery } from 'react-apollo-hooks';
+import { UserEmailsDocument, UserEmailsQuery } from '../../generated-models';
 
-interface EmailListProps {
-  users: UserEmailsAllUsers[];
-  loading: boolean;
-}
-
-const EmailList: React.FC<EmailListProps> = ({ users, loading }) => (
-  <div>
-    <h1>Users that signed up in the last week</h1>
-    {loading ? (
-      <LoadingIndicator />
-    ) : users.length > 0 ? (
-      users.map(user => <div key={user.id}>{user.email}</div>)
-    ) : (
-      <p>No new users signed up in the last week</p>
-    )}
-  </div>
-);
-
-const EmailListPage = UserEmailsHOC({
-  options: {
-    variables: {
-      date: format(subDays(new Date(), 7), 'YYYY-MM-DDTHH:mm:ss.SSSZ')
+const EmailListPage: React.FC = () => {
+  const date7DaysAgo = useMemo(
+    () => format(subDays(new Date(), 7), 'YYYY-MM-DDTHH:mm:ss.SSSZ'),
+    []
+  );
+  const { data, error, loading } = useQuery<UserEmailsQuery>(
+    UserEmailsDocument,
+    {
+      variables: {
+        date: date7DaysAgo
+      }
     }
-  },
-  props: props => {
-    const { allUsers = [], loading = true } = props.data || {};
-    return { users: allUsers, loading };
+  );
+  if (loading || !data || !data.allUsers) {
+    return <LoadingIndicator />;
   }
-})(EmailList);
+  if (error) {
+    return <div>Error! {error.message}</div>;
+  }
+  const users = data.allUsers;
+
+  return (
+    <div>
+      <h1>Users that signed up in the last week</h1>
+      {users.length > 0 ? (
+        users.map(user => <div key={user.id}>{user.email}</div>)
+      ) : (
+        <p>No new users signed up in the last week</p>
+      )}
+    </div>
+  );
+};
 
 export default EmailListPage;
